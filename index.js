ac79b5dadbfc88581317bf37828011fe4320c03e
Chamak Karmakar <chamakkarmakar.ck@gmail.com>
2022-08-17 15:46:30 +0600

    database connected

--- index.js
+++ index.js
@@ -2,10 +2,27 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const app = express();
+require('dotenv').config();
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.g7iwftr.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run () {
+    try {
+        await client.connect();
+        const itemCollection = client.db('dbItemInfo').collection('items');
+        console.log('stock management database connected');
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 // Root API
 app.get('/',(req,res) => {
